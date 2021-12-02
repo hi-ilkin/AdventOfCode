c5824df152c7e0009cf4c55e187920e08e1078d4
ilkin <huseynlilkin@gmail.com>
2021-12-02 12:19:08 +0100

    added solution for day 2 part 2

--- day-2/solution.js
+++ day-2/solution.js
@@ -1,40 +1,66 @@
-let vertical_pos = 0;
-let horizontal_pos = 0;
-
 // Include fs module
 const fs = require('fs');
 
-// Calling the readFileSync() method
-// to read 'input.txt' file
 const data = fs.readFileSync('input.txt',
   { encoding: 'utf8', flag: 'r' });
 
-// Display the file data
+function solve_part_1(data) {
+  var vertical_pos = 0;
+  var horizontal_pos = 0;
 
-console.log();
+  data.forEach(line => {
+    var [direction, value] = line.split(" ");
+    value = Number(value);
 
-data.split('\n').forEach(line => {
-  var [direction, value] = line.split(" ");
-  value = Number(value);
-  console.log(direction, '=>', value, typeof(value));
+    switch (direction) {
+      case 'forward':
+        horizontal_pos += value;
+        return;
 
-  switch (direction) {
-    case 'forward':
-      horizontal_pos += value;
-      return;
+      case 'up':
+        vertical_pos -= value;
+        return;
 
-    case 'up':
-      vertical_pos -= value;
-      return;
+      case 'down':
+        vertical_pos += value;
+        return;
+      default:
+        return;
+    }
+  });
 
-    case 'down':
-      vertical_pos += value;
-      return;
-    default:
-      console.log('problem');
-      return;
-  }
-  console.log('------------');
-});
+  return [vertical_pos, horizontal_pos];
+}
 
-console.log(vertical_pos, horizontal_pos, vertical_pos * horizontal_pos);
+
+function solve_part_2(data) {
+  var aim = 0;
+  var horizontal_pos = 0;
+  var vertical_pos = 0;
+
+  data.forEach(line => {
+    var [direction, value] = line.split(" ");
+
+    value = Number(value);
+    switch (direction) {
+      case 'up':
+        aim -= value;
+        return;
+      case 'down':
+        aim += value;
+        return;
+      case 'forward':
+        horizontal_pos += value;
+        vertical_pos += aim * value;
+        return;
+      default:
+        return;
+    }
+
+  });
+
+  return [vertical_pos, horizontal_pos];
+}
+
+var [v, h] = solve_part_2(data.split('\n'));
+console.log(v, h, v * h);
