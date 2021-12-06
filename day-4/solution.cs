a3b417cb9002538b736c1c216cf8dd048b0fe955
ilkin <huseynlilkin@gmail.com>
2021-12-06 17:32:24 +0100

    added solution for day 4 part 2

--- day-4/solution.cs
+++ day-4/solution.cs
@@ -6,6 +6,7 @@ namespace day_4
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines("input.txt");
@@ -29,31 +30,60 @@ namespace day_4
             }
             boards.Add(board);
 
-
             Console.WriteLine($"Number of Numbers: {drawnNumbers.Length}");
             Console.WriteLine($"Number of Boards: {boards.Count}");
             bool isGameOver = false;
             foreach (int n in drawnNumbers)
             {
+                List<int> winnerBoardIds = new List<int>();
                 Console.Write($"{n}, ");
-                for(int i=0; i < boards.Count; i++)
+                for (int i = 0; i < boards.Count; i++)
                 {
                     Board b = boards[i];
                     b.checkNumber(n);
                     if (b.isWinner() == true)
                     {
-                        Console.WriteLine();
-                        b.print();
-                        int reward = b.getReward(n);
-                        Console.WriteLine($"Reward of board {i}: {reward}");
-                        isGameOver = true;
-                        break;
+                        // Console.WriteLine();
+                        // b.print();
+                        // int reward = b.getReward(n);
+                        // Console.WriteLine($"Reward of board {i}: {reward}");
+
+                        // Console.WriteLine("************");
+                        winnerBoardIds.Add(i);
+                        if (boards.Count == 1)
+                        {
+                            Console.WriteLine();
+                            b.print();
+                            int reward = b.getReward(n);
+                            Console.WriteLine($"Reward of board {i}: {reward}");
+                            isGameOver = true;
+                            break;
+                        }
                     }
                 }
                 if (isGameOver == true)
                 {
                     break;
                 }
+                try
+                {
+                    for (int idx = winnerBoardIds.Count - 1; idx >= 0; idx--)
+                    {
+                        boards.RemoveAt(winnerBoardIds[idx]);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("-------------------");
+                    Console.WriteLine(e);
+                    foreach (int idx in winnerBoardIds)
+                    {
+                        Console.WriteLine(idx);
+                    }
+                }
+
+
+
             }
         }
     }
